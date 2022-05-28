f8adfc939c0e9c796e01e3f1f96501bce0dbd393
Gridy <reality023@naver.com>
2022-05-28 23:00:17 +0900

    #4.3 configureStore

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {createStore} from "redux";
-import {createAction, createReducer} from "@reduxjs/toolkit";
+import {configureStore, createAction, createReducer} from "@reduxjs/toolkit";
 
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
@@ -24,7 +24,7 @@ const reducer = createReducer([], {
   }
 });
 
-const store = createStore(reducer);
+const store = configureStore({reducer});
 
 export const actionCreators = {
   addTodo,
